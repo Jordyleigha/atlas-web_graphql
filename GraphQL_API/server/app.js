4026b046777213b758d159131c1cb77a471f1fba
Jordan Arguelles <Jordan.arguelles.ja@gmail.com>
2025-11-12 23:14:06 -0600

    finishing

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const schema = require('./schema/schema.js');
 const { graphqlHTTP } = require('express-graphql');
+const mongoose = require('mongoose');
 
 const app = express();
+mongoose.connect('mongodb+srv://atlas-sql-69155b23b14067138557252f-0vlhk5.a.query.mongodb.net/sample_mflix?ssl=true&authSource=admin');
 
 app.use('/graphql', graphqlHTTP({
   schema,
